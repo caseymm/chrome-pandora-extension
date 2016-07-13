281690e3063c812daf5b37c17aa2992ad2304484
caseymm <casey.miller@voxmedia.com>
2016-07-12 19:26:37 -0500

    make pandora pretty

--- contentscript.js
+++ contentscript.js
@@ -1,92 +1,80 @@
 $(document).ready(function() {
-  var live_hours,
-      first_col;
+  var spotifyApi = new SpotifyWebApi();
+  console.log('loading Pandora extension!');
+  var container = $('#mainContentContainer');
+  $('#topnav').after(container);
+  $('.skinContainer').hide();
+  $('body').append('<img class="spotify" width="175" height="175" src="" crossOrigin="anonymous" style="display: inline;">');
+  $('body').append('<div id="blocks"></div>');
 
-  function addWaiting(){
-    var all_members = $(first_col).find('img.member-avatar');
-    live_hours = {};
-    $.each(all_members, function(i,d){
-      var card_title = $(d).parent().parent().parent().find('.list-card-title').html().split("</span>")[1];
-      var hours = parseFloat($(d).parent().parent().parent().find('.badge.badge-points.point-count').first().html());
-      var member_name = $(d).attr('title');
-      if(isNaN(hours)){
-        hours = 0;
-      }
-      if(!(live_hours[member_name])){
-        live_hours[member_name] = {'total': hours || 0, 'tasks': []};
-        live_hours[member_name]['tasks'].push([card_title, hours]);
-      } else {
-        live_hours[member_name]['total'] += hours;
-        live_hours[member_name]['tasks'].push([card_title, hours]);
-      }
-    })
-    appendHours();
-  }
+  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-  function appendHours(){
-    $.each($('.member.ui-draggable'), function(i, d){
-      var sidebar_member = $(d).find('img.member-avatar').attr('title');
-      if(live_hours[sidebar_member]){
-        var current_height = $(d).height();
-        if(current_height === 30){
-          $(d).height(current_height+25);
-        }
-        $(d).removeClass('hold-up');
-        $(d).removeClass('caution');
-        $(d).removeClass('all-good');
-        var set_class = 'all-good';
-        if(live_hours[sidebar_member]['total'] > 40){
-          $(d).addClass('hold-up');
-          set_class = 'caution';
-        } else if(live_hours[sidebar_member]['total'] > 32){
-          set_class = 'caution';
-        }
-        $(d).addClass(set_class);
+  var observer = new MutationObserver(function(mutations, observer) {
+    // fired when a mutation occurs
+    // console.log(mutations, $('.playerBarArt')[0], $('.playerBarArt').attr('src'));
+    var albumName = $('.trackData .albumTitle').text();
+    // console.log(albumName);
+    // spotifyApi.searchAlbums(albumName)
+    //   .then(function(data) {
+    //     console.log(albumName, data);
+    //   }, function(err) {
+    //     console.error(err);
+    // });
 
-        $(d).append('<div class="hour-ct"></div>');
-        var f_num = String(live_hours[sidebar_member]['total']).replace('0.', '.');
-        $(d).find('.hour-ct').html(f_num);
-        $(d).mouseenter(function(event, ui){
-          var position = $(d).position();
-          var p_top = position['top'];
-          var p_left = ($('.board-main-content').width() + position['left']) - 207;
-          $('#tooltip').css({'display': 'block', 'top': String(p_top)+'px', 'left': String(p_left)+'px'});
-          $('#tooltip #member-name').html(sidebar_member.split(' (')[0]+' '+'('+live_hours[sidebar_member]['tasks'].length+')');
+    var artist = $('.info .playerBarArtist').text();
+    console.log(artist);
+    spotifyApi.searchArtists(artist)
+      .then(function(data) {
+        // console.log(artist, data.artists.items);
+        var match = false,
+            count = 0,
+            albumImageUrl = '';
+        while (!(match) && count < data.artists.items.length){
+          console.log(data.artists.items[count]);
+          spotifyApi.getArtistAlbums(data.artists.items[count]['id'], {limit: 50}).then(function(data){
+            var nAlbumName = albumName.toLowerCase().replace('\'', '').split(' (')[0];
+            // map each data items so that they don't inclue the parens either
+            _.each(data.items, function(item){
+              console.log(item);
+              item.name = item.name.toLowerCase().replace('\'', '').split(' (')[0];
+            })
+            console.log(nAlbumName, data.items, _.findWhere(data.items, {name: nAlbumName}));
+            var found = _.findWhere(data.items, {name: nAlbumName});
+            if(found){
+              match = true;
+              albumImageUrl = found.images[0]['url'];
 
-          live_hours[sidebar_member]['tasks'].sort(function(first, second) {
-            return second[1] - first[1];
+              $('img.spotify').attr('src', albumImageUrl);
+              $('img.spotify').load(function(){
+                $('#blocks').empty();
+                var colorThief = new ColorThief();
+                var myImage = $('img.spotify')[0];
+                var colors = colorThief.getPalette(myImage, 4);
+                var colorArr = [];
+                console.log(colors);
+                $.each(colors, function(idx, color){
+                  console.log(color);
+                  var rgbColor = 'rgb('+color[0]+','+color[1]+','+color[2]+')';
+                  colorArr.push(rgbColor);
+                  $('#blocks').append('<div style="width: 20px; height: 20px; margin:5px; background-color: '+rgbColor+';"></div>');
+                });
+                console.log(colorArr.join(', '));
+                $('body').attr('style', 'background: linear-gradient(270deg, '+colorArr.join(', ')+');');
+              });
+            }
           });
-          $.each(live_hours[sidebar_member]['tasks'], function(i, d){
-            $('#tooltip #tasks').append('<tr id="tr-'+i+'"></tr>');
-            $('#tr-'+i).append('<td class="hour-block">'+d[1]+'</td>');
-            $('#tr-'+i).append('<td class="item-task">'+d[0]+'</td>');
-          })
-        });
-        $(d).mouseleave(function(){
-          $('#tooltip').css('display', 'none');
-          $('#tooltip #tasks').find('tr').remove();
-        });
-      } else{
-        $(d).css('margin-bottom', '30px');
-      }
-      $(d).click(function(){
-        addWaiting();
+          count += 1;
+        };
       });
-    })
-  }
+      }, function(err) {
+        console.error(err);
+    });
 
-  setTimeout(function(){
-    $('#content').append('<div id="tooltip"><div id="member-name"></div><table id="tasks"></table></div>')
-    first_col = $('.js-list.list-wrapper').first().attr('id', 'first-col');
-    addWaiting();
-  }, 2000);
-
-  $('#first-col .list-card').click(function(){
-    addWaiting();
-  });
-
-  $('.window-overlay').click(function(){
-    addWaiting();
+  // define what element should be observed by the observer
+  // and what types of mutations trigger the callback
+  observer.observe($('.nowplaying')[0], {
+    subtree: true,
+    childList: true
   });
 
 });
